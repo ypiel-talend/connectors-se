b4ffecd9b4491276b8aeff44195bac07355641c3
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-06-11 16:47:57 +0200

    starting bigquery

---
+++ bigquery/src/main/java/org/talend/components/bigquery/package-info.java
@@ -0,0 +1,8 @@
+@Icon(BIGQUERY)
+@Components(family = "BigQuery")
+package org.talend.components.bigquery;
+
+import static org.talend.sdk.component.api.component.Icon.IconType.BIGQUERY;
+
+import org.talend.sdk.component.api.component.Components;
+import org.talend.sdk.component.api.component.Icon;
