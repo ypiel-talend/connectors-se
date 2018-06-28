24865a33a9d3ace0f66ea76605fecc020a2e8e7b
Chen Bin <bchen@talend.com>
2018-06-28 16:12:49 +0800

    fix(TFD-4805): rewrite the runtime of DevNull

--- localio/src/main/java/org/talend/components/localio/devnull/DevNullOutput.java
+++ localio/src/main/java/org/talend/components/localio/devnull/DevNullOutput.java
@@ -6,6 +6,12 @@ import java.io.Serializable;
 
 import javax.json.JsonObject;
 
+import org.apache.avro.generic.IndexedRecord;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.values.PBegin;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
+import org.talend.components.localio.fixed.FixedDataSetConfiguration;
 import org.talend.sdk.component.api.component.Icon;
 import org.talend.sdk.component.api.component.Version;
 import org.talend.sdk.component.api.configuration.Option;
@@ -17,14 +23,19 @@ import org.talend.sdk.component.api.processor.Processor;
 @Icon(TRASH)
 @Processor(name = "DevNullOutput")
 @Documentation("This component ignores any input.")
-public class DevNullOutput implements Serializable {
+public class DevNullOutput extends PTransform<PCollection<IndexedRecord>, PCollection<IndexedRecord>> {
 
-    public DevNullOutput(@Option("configuration") final DevNullDataSet configuration) {
+    public DevNullOutput(@Option("configuration") final FixedDataSetConfiguration configuration) {
         // no-op
     }
 
     @ElementListener
     public void onElement(final JsonObject ignored) {
         // no-op
+    }
+
+    @Override
+    public PCollection<IndexedRecord> expand(PCollection<IndexedRecord> input) {
+        return input;
     }
 }
