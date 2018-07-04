344f4a9b2cd154c23374541184d2fe5a2cc39525
Ryan Skraba <rskraba@talend.com>
2018-07-04 17:21:08 +0200

    fix: Update generated talend/component-server:1.0.2-sprint26-connectorsIncluded

--- build_component_server_docker.sh
+++ build_component_server_docker.sh
@@ -23,7 +23,7 @@ docker build --tag $__image --build-arg BUILD_VERSION=${__version} .
 CONNECTORS_SE_TAG_SUGGEST=$(echo $__version | sed "s/SNAPSHOT/dev/")
 COMPONENT_SERVER_TAG=${COMPONENT_SERVER_TAG:-latest}
 
-docker build --tag talend/component-server:1.0.1-sprint26-connectorsIncluded \
+docker build --tag talend/component-server:1.0.2-sprint26-connectorsIncluded \
 	--build-arg CONNECTORS_SE_TAG=${CONNECTORS_SE_TAG:-$CONNECTORS_SE_TAG_SUGGEST} \
 	--build-arg COMPONENT_SERVER_TAG=$COMPONENT_SERVER_TAG \
 	-f component-server-docker/Dockerfile .
