a40674603de725798afe4f576345baa7d7387f8f
vizotenko <vizotenko@talend.com>
2018-07-31 17:47:35 +0300

    feat(TDI-40741): Implement TaCoKit JMS component

--- jms/src/main/java/org/talend/components/jms/source/InputSource.java
+++ jms/src/main/java/org/talend/components/jms/source/InputSource.java
@@ -6,6 +6,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.input.Producer;
@@ -16,17 +17,19 @@ import org.talend.components.jms.service.JmsService;
 @Documentation("TODO fill the documentation for this source")
 public class InputSource implements Serializable {
 
+    public static final String MESSAGE_CONTENT = "messageContent";
     private final InputMapperConfiguration configuration;
 
     private final JmsService service;
 
     private final JsonBuilderFactory jsonBuilderFactory;
 
-    public InputSource(@Option("configuration") final InputMapperConfiguration configuration, final JmsService service,
+    public InputSource(@Option("configuration1") final InputMapperConfiguration configuration, final JmsService service,
             final JsonBuilderFactory jsonBuilderFactory) {
         this.configuration = configuration;
         this.service = service;
         this.jsonBuilderFactory = jsonBuilderFactory;
+        service.setConfiguration(configuration);
     }
 
     @PostConstruct
@@ -38,11 +41,18 @@ public class InputSource implements Serializable {
     @Producer
     public JsonObject next() {
         // this is the method allowing you to go through the dataset associated
-        // to the component configuration
+        // to the component configuration1
         //
         // return null means the dataset has no more data to go through
         // you can use the jsonBuilderFactory to create new JsonObjects.
-        return null;
+        String message = service.receiveTextMessage();
+        return message != null ? buildJSON(message) : null;
+    }
+
+    private JsonObject buildJSON(String text) {
+        JsonObjectBuilder recordBuilder = jsonBuilderFactory.createObjectBuilder();
+        recordBuilder.add(MESSAGE_CONTENT, text);
+        return recordBuilder.build();
     }
 
     @PreDestroy
--- jms/src/test/java/org/talend/components/jms/service/JMSServiceTest.java
+++ jms/src/test/java/org/talend/components/jms/service/JMSServiceTest.java
@@ -1,16 +1,11 @@
 package org.talend.components.jms.service;
 
-import org.apache.activemq.command.ActiveMQTextMessage;
 import org.junit.jupiter.api.Test;
+import org.talend.components.jms.configuration.MessageType;
 import org.talend.components.jms.output.OutputConfiguration;
+import org.talend.components.jms.source.InputMapperConfiguration;
 import org.talend.sdk.component.api.service.Service;
 import org.talend.sdk.component.junit5.WithComponents;
-
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageNotWriteableException;
-import javax.jms.MessageProducer;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,36 +18,33 @@ public class JMSServiceTest {
 
     public static final String TEST_MESSAGE = "hello world";
 
-    @Service
-    private OutputConfiguration configuration;
-
     public static final String ACTIVEMQ = "ACTIVEMQ";
 
     @Service
     private JmsService jmsService;
 
     @Test
-    void sendAndReceiceJMSMessage() throws JMSException {
+    void sendAndReceiceJMSMessage() {
         OutputConfiguration configuration = new OutputConfiguration();
         configuration.setModuleList(ACTIVEMQ);
         configuration.setUrl(URL);
-        configuration.setTo(QUEUE_TEST);
-        configuration.setMessageType(OutputConfiguration.MessageType.QUEUE);
+        configuration.setDestination(QUEUE_TEST);
+        configuration.setMessageType(MessageType.QUEUE);
         jmsService.setConfiguration(configuration);
-        MessageProducer producer = jmsService.createProducer();
-        producer.send(createActiveMQMessage(TEST_MESSAGE));
+        jmsService.sendTextMessage(TEST_MESSAGE);
 
-        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, ((ActiveMQTextMessage) jmsService.receiveMessage()).getText());
+        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage());
     }
 
-    private Message createActiveMQMessage(String text) {
-        Message message = new ActiveMQTextMessage();
-        try {
-            ((ActiveMQTextMessage) message).setText(text);
-        } catch (MessageNotWriteableException e) {
-            e.printStackTrace();
-        }
-        return message;
-    }
+    @Test
+    void receiveJMSMessage() {
+        InputMapperConfiguration configuration = new InputMapperConfiguration();
+        configuration.setModuleList(ACTIVEMQ);
+        configuration.setUrl(URL);
+        configuration.setDestination(QUEUE_TEST);
+        configuration.setMessageType(MessageType.QUEUE);
+        jmsService.setConfiguration(configuration);
+        String message = jmsService.receiveTextMessage();
 
+    }
 }
