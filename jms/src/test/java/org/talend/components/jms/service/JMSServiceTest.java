5ec9ed2658990c4c2ca120dae95f7e89bc1d11a0
vizotenko <vizotenko@talend.com>
2018-08-03 15:39:19 +0300

    feat(TDI-40741): Implement TaCoKit JMS component

--- jms/src/test/java/org/talend/components/jms/service/JMSServiceTest.java
+++ jms/src/test/java/org/talend/components/jms/service/JMSServiceTest.java
@@ -3,6 +3,7 @@ package org.talend.components.jms.service;
 import org.junit.jupiter.api.Test;
 import org.talend.components.jms.configuration.MessageType;
 import org.talend.components.jms.output.OutputConfiguration;
+import org.talend.components.jms.source.DurableSubscriptionConfiguration;
 import org.talend.components.jms.source.InputMapperConfiguration;
 import org.talend.sdk.component.api.service.Service;
 import org.talend.sdk.component.junit5.WithComponents;
@@ -22,59 +23,92 @@ public class JMSServiceTest {
 
     public static final boolean USER_IDENTITY = true;
 
+    public static final boolean DURABLE_SUBSCRIPTION = false;
+
+    public static final String CLIENT_ID = "ClientId";
+
+    public static final String SUBSCRIBER_NAME = "subscrName";
+
+    public static final int TIMEOUT = 1;
+
     @Service
     private JmsService jmsService;
 
     @Test
     void sendAndReceiveJMSMessageQueue() {
+        jmsService.setConfiguration(getOutputConfiguration());
+        jmsService.sendTextMessage(TEST_MESSAGE);
+
+        jmsService.setConfiguration(getInputConfiguration());
+        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage());
+    }
+
+    @Test
+    void sendJMSMessageTopic() {
+        OutputConfiguration configuration = new OutputConfiguration();
+        configuration.setModuleList(ACTIVEMQ);
+        configuration.setUrl(URL);
+        configuration.setDestination(DESTINATION);
+        configuration.setMessageType(MessageType.TOPIC);
+        jmsService.setConfiguration(configuration);
+        jmsService.sendTextMessage(TEST_MESSAGE);
+
+        //assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage());
+    }
+
+    @Test
+    void receiveJMSMessageTopic() {
+        InputMapperConfiguration configuration = getInputConfiguration();
+        configuration.setModuleList(ACTIVEMQ);
+        configuration.setUrl(URL);
+        configuration.setDestination(DESTINATION);
+        configuration.setMessageType(MessageType.TOPIC);
+        configuration.getSubscriptionConfig().setDurableSubscription(true);
+        configuration.setTimeout(0);
+
+        jmsService.setConfiguration(configuration);
+        // jmsService.sendTextMessage(TEST_MESSAGE);
+
+        System.out.println(jmsService.receiveTextMessage());;
+//        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage());
+    }
+
+
+    private OutputConfiguration getOutputConfiguration() {
         OutputConfiguration configuration = new OutputConfiguration();
         configuration.setModuleList(ACTIVEMQ);
         configuration.setUrl(URL);
         configuration.setDestination(DESTINATION);
         configuration.setMessageType(MessageType.QUEUE);
-        jmsService.setConfiguration(configuration);
-        jmsService.sendTextMessage(TEST_MESSAGE);
-
-        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage(3000));
+        return configuration;
     }
 
-    @Test
-    void sendJMSMessageTopic() {
+    private InputMapperConfiguration getInputConfiguration() {
         InputMapperConfiguration configuration = new InputMapperConfiguration();
         configuration.setModuleList(ACTIVEMQ);
-        configuration.setUrl(URL);
-        configuration.setDestination(DESTINATION);
-        configuration.setMessageType(MessageType.TOPIC);
-        jmsService.setConfiguration(configuration);
-        jmsService.sendTextMessage(TEST_MESSAGE);
-
-         assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage(3000));
-    }
-
-    @Test
-    void receiveJMSMessageTopic() {
-        InputMapperConfiguration configuration = new InputMapperConfiguration();
-        configuration.setModuleList(ACTIVEMQ);
-        configuration.setUrl(URL);
-        configuration.setDestination(DESTINATION);
-        configuration.setMessageType(MessageType.TOPIC);
-        jmsService.setConfiguration(configuration);
-        // jmsService.sendTextMessage(TEST_MESSAGE);
-
-        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage(3000));
-    }
-
-    @Test
-    void sendJMSMessageWithUserIdentity() {
-        InputMapperConfiguration configuration = new InputMapperConfiguration();
-        configuration.setModuleList(ACTIVEMQ);
-        configuration.setUserIdentity(true);
         configuration.setUrl(URL);
         configuration.setDestination(DESTINATION);
         configuration.setMessageType(MessageType.QUEUE);
-        jmsService.setConfiguration(configuration);
-        jmsService.sendTextMessage(TEST_MESSAGE);
-
-        assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage(3000));
+        DurableSubscriptionConfiguration subsConfig = new DurableSubscriptionConfiguration();
+        subsConfig.setDurableSubscription(DURABLE_SUBSCRIPTION);
+        subsConfig.setClientId(CLIENT_ID);
+        subsConfig.setSubscriberName(SUBSCRIBER_NAME);
+        configuration.setSubscriptionConfig(subsConfig);
+        configuration.setTimeout(TIMEOUT);
+        return configuration;
     }
+
+     @Test
+     void sendJMSMessageWithUserIdentity() {
+     InputMapperConfiguration configuration = new InputMapperConfiguration();
+     configuration.setModuleList(ACTIVEMQ);
+     configuration.setUserIdentity(true);
+     configuration.setUrl(URL);
+     configuration.setDestination(DESTINATION);
+     configuration.setMessageType(MessageType.QUEUE);
+     jmsService.setConfiguration(configuration);
+     jmsService.sendTextMessage(TEST_MESSAGE);
+
+     assertEquals("Sent and received messages should be equal", TEST_MESSAGE, jmsService.receiveTextMessage());
+     }
 }
