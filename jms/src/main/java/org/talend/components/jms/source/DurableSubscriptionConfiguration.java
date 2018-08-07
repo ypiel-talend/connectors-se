7d97144eb9290e646062ff9aa620ffb28ddbd529
vizotenko <vizotenko@talend.com>
2018-08-07 16:51:20 +0300

    feat(TDI-40741): Implement TaCoKit JMS component

--- jms/src/main/java/org/talend/components/jms/source/DurableSubscriptionConfiguration.java
+++ jms/src/main/java/org/talend/components/jms/source/DurableSubscriptionConfiguration.java
@@ -3,14 +3,14 @@ package org.talend.components.jms.source;
 import lombok.Data;
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.configuration.condition.ActiveIf;
-import org.talend.sdk.component.api.configuration.type.DataStore;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.meta.Documentation;
 
 import java.io.Serializable;
 
-@DataStore("durableSubscription")
-@GridLayout({ @GridLayout.Row({ "durableSubscription" }), @GridLayout.Row({ "clientId", "subscriberName" }) })
+@GridLayout({
+        @GridLayout.Row({"durableSubscription"}),
+        @GridLayout.Row({"clientId", "subscriberName"})})
 @Data
 public class DurableSubscriptionConfiguration implements Serializable {
 
--- jms/src/main/java/org/talend/components/jms/source/InputSource.java
+++ jms/src/main/java/org/talend/components/jms/source/InputSource.java
@@ -1,23 +1,42 @@
 package org.talend.components.jms.source;
 
 import java.io.Serializable;
+import java.util.Hashtable;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.naming.Context;
+import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
 
+import lombok.extern.slf4j.Slf4j;
+import org.talend.components.jms.configuration.MessageType;
+import org.talend.components.jms.service.I18nMessage;
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.input.Producer;
 import org.talend.sdk.component.api.meta.Documentation;
 
 import org.talend.components.jms.service.JmsService;
 
+@Slf4j
 @Documentation("TODO fill the documentation for this source")
 public class InputSource implements Serializable {
 
     public static final String MESSAGE_CONTENT = "messageContent";
+
+    private final static String CONNECTION_FACTORY = "ConnectionFactory";
 
     private final InputMapperConfiguration configuration;
 
@@ -27,35 +46,99 @@ public class InputSource implements Serializable {
 
     private int counter;
 
-    public InputSource(@Option("configuration1") final InputMapperConfiguration configuration, final JmsService service,
-            final JsonBuilderFactory jsonBuilderFactory) {
+    private Connection connection;
+
+    private Context jndiContext;
+
+    private Session session;
+
+    private Destination destination;
+
+    private MessageConsumer consumer;
+
+    private final I18nMessage i18n;
+
+    public InputSource(@Option("configuration") final InputMapperConfiguration configuration, final JmsService service,
+                       final JsonBuilderFactory jsonBuilderFactory, final I18nMessage i18nMessage) {
         this.configuration = configuration;
         this.service = service;
         this.jsonBuilderFactory = jsonBuilderFactory;
-        service.setConfiguration(configuration);
+        this.i18n = i18nMessage;
     }
 
     @PostConstruct
     public void init() {
-        // this method will be executed once for the whole component execution,
-        // this is where you can establish a connection for instance
+        try {
+            // create JNDI context
+            jndiContext = service.getJNDIContext(configuration.getBasicConfig().getConnection().getUrl(),
+                    configuration.getBasicConfig().getConnection().getModuleList()
+            );
+            // create ConnectionFactory from JNDI
+            ConnectionFactory connectionFactory = service.getConnectionFactory(jndiContext);
+
+            try {
+                connection = service.getConnection(connectionFactory,
+                        configuration.getBasicConfig().getConnection().isUserIdentity(),
+                        configuration.getBasicConfig().getConnection().getUserName(),
+                        configuration.getBasicConfig().getConnection().getPassword());
+            } catch (JMSException e) {
+                throw new IllegalStateException(i18n.errorInvalidConnection());
+            }
+
+            if (configuration.getSubscriptionConfig().isDurableSubscription()) {
+                connection.setClientID(configuration.getSubscriptionConfig().getClientId());
+            }
+
+            try {
+                connection.start();
+            } catch (JMSException e) {
+                throw new IllegalStateException(i18n.errorStartMessagesDelivery());
+            }
+
+            session = service.getSession(connection);
+
+            destination = service.getDestination(session,
+                    jndiContext,
+                    configuration.getBasicConfig().getDestination(),
+                    configuration.getBasicConfig().getMessageType(),
+                    configuration.getBasicConfig().isUserJNDILookup());
+
+            if (configuration.getSubscriptionConfig().isDurableSubscription()) {
+                consumer = session.createDurableSubscriber((Topic) destination,
+                        configuration.getSubscriptionConfig().getSubscriberName(), configuration.getMessageSelector(), false);
+            } else {
+                consumer = session.createConsumer(destination, configuration.getMessageSelector());
+            }
+
+        } catch (JMSException e) {
+            throw new IllegalStateException(i18n.errorCreateJMSInstance());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NamingException e) {
+            throw new IllegalStateException(i18n.errorInstantiateConnectionFactory(e.getMessage()));
+        }
     }
 
     @Producer
     public JsonObject next() {
-        // this is the method allowing you to go through the dataset associated
-        // to the component configuration1
-        //
-        // return null means the dataset has no more data to go through
-        // you can use the jsonBuilderFactory to create new JsonObjects.
-        String message;
+
+        String textMessage = null;
         if (counter >= configuration.getMaximumMessages()) {
             return null;
         } else {
-            message = service.receiveTextMessage();
+
+            Message message = null;
+            try {
+                message = consumer.receive(configuration.getTimeout() * 1000);
+                if (message != null) {
+                    textMessage = ((TextMessage) message).getText();
+                    message.acknowledge();
+                }
+            } catch (JMSException e) {
+                throw new IllegalStateException(i18n.errorCantReceiveMessage());
+            }
+            counter++;
+            return message != null ? buildJSON(textMessage) : null;
+
         }
-        counter++;
-        return message != null ? buildJSON(message) : null;
     }
 
     private JsonObject buildJSON(String text) {
@@ -66,7 +149,9 @@ public class InputSource implements Serializable {
 
     @PreDestroy
     public void release() {
-        // this is the symmetric method of the init() one,
-        // release potential connections you created or data you cached
+        service.closeConsumer(consumer);
+        service.closeSession(session);
+        service.closeConnection(connection);
+        service.closeContext(jndiContext);
     }
 }
