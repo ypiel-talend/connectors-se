40fff5ada82c5415addd722326ebc0e502f5e5b8
ypiel <ypiel@talend.com>
2018-08-08 18:32:03 +0200

    Name is set in docker image even for master

--- connectors-se-docker/src/main/scripts/docker/common.sh
+++ connectors-se-docker/src/main/scripts/docker/common.sh
@@ -23,11 +23,7 @@ export CONNECTOR_VERSION=$(grep "<version>" "$BASEDIR/pom.xml" | head -n 1 | sed
 export TALEND_REGISTRY="${TALEND_REGISTRY:-registry.datapwn.com}"
 DOCKER_IMAGE_VERSION=${DOCKER_IMAGE_VERSION:-$CONNECTOR_VERSION}
 if [[ "$DOCKER_IMAGE_VERSION" = *"SNAPSHOT" ]]; then
-
-    BRANCH="$1"
-    [ "master" = "$BRANCH" ] && BRANCH=""
-    [ -n "$BRANCH" ] && BRANCH=_${BRANCH}
-
+    BRANCH=_${1}
     DOCKER_IMAGE_VERSION=$(echo $CONNECTOR_VERSION | sed "s/-SNAPSHOT//")${BRANCH}_$(date +%Y%m%d%I%M%S)
 fi
 export DOCKER_IMAGE_VERSION
