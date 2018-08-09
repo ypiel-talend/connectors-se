ad2cdd259bb12ea21006a1e613577e65fdcba71b
vizotenko <vizotenko@talend.com>
2018-08-09 16:23:36 +0300

    feat(TDI-40741): Implement TaCoKit JMS component

--- jms/src/main/java/org/talend/components/jms/configuration/BasicConfiguration.java
+++ jms/src/main/java/org/talend/components/jms/configuration/BasicConfiguration.java
@@ -1,7 +1,6 @@
 package org.talend.components.jms.configuration;
 
 import lombok.Data;
-import org.talend.components.jms.datastore.JmsDataStore;
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.meta.Documentation;
@@ -10,20 +9,10 @@ import java.io.Serializable;
 
 @Data
 @GridLayout(value = {
-        @GridLayout.Row({"connection"}),
-        @GridLayout.Row({"userJNDILookup"}),
         @GridLayout.Row({"messageType"}),
         @GridLayout.Row({"destination"})},
         names = GridLayout.FormType.MAIN)
 public class BasicConfiguration implements Serializable {
-
-    @Option
-    @Documentation("JMS connection information")
-    private JmsDataStore connection;
-
-    @Option
-    @Documentation("Checkbox for JNDI Name Lookup Destination")
-    private boolean userJNDILookup = false;
 
     @Option
     @Documentation("Drop down list for Message Type")
