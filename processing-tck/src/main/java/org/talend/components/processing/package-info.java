f70073d04cb69dddfe08055dfa3db9c819d1e10d
Ryan Skraba <rskraba@talend.com>
2018-08-21 17:34:53 +0200

    fix(TFD-4606): Separate Beam and TCK/JSON processors.

---
+++ processing-tck/src/main/java/org/talend/components/processing/package-info.java
@@ -0,0 +1,6 @@
+@Icon(Icon.IconType.FILTER)
+@Components(family = "Processing")
+package org.talend.components.processing;
+
+import org.talend.sdk.component.api.component.Components;
+import org.talend.sdk.component.api.component.Icon;
