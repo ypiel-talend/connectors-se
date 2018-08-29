97914a1e1e8bc4ca3a7fc43588601624a097ddad
s.bovsunovskyi <s.bovsunovskyi@globallogic.com>
2018-08-29 22:09:20 +0300

    do not init HttpClient with every request

--- magentocms/src/main/java/org/talend/components/magentocms/service/http/MagentoHttpServiceFactory.java
+++ magentocms/src/main/java/org/talend/components/magentocms/service/http/MagentoHttpServiceFactory.java
@@ -1,6 +1,6 @@
 package org.talend.components.magentocms.service.http;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import oauth.signpost.commonshttp.HttpRequestAdapter;
 import oauth.signpost.exception.OAuthCommunicationException;
@@ -40,35 +40,32 @@ public class MagentoHttpServiceFactory {
     @Service
     private JsonParserFactory jsonParserFactory;
 
-    @AllArgsConstructor
+    @RequiredArgsConstructor
     public class MagentoHttpService {
 
-        private MagentoCmsConfigurationBase magentoCmsConfigurationBase;
+        private final MagentoCmsConfigurationBase magentoCmsConfigurationBase;
+
+        private CloseableHttpClient httpclient = HttpClients.createDefault();
 
         public List<JsonObject> getRecords(String magentoUrl)
                 throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException,
                 UnknownAuthenticationTypeException, BadRequestException, BadCredentialsException {
-            CloseableHttpClient httpclient = HttpClients.createDefault();
+            List<JsonObject> dataList;
             try {
-                List<JsonObject> dataList;
+                dataList = execGetRecords(httpclient, magentoUrl);
+                return dataList;
+            } catch (UserTokenExpiredException e) {
+                // try to get new token
+                AuthenticationLoginPasswordSettings authSettings = (AuthenticationLoginPasswordSettings) magentoCmsConfigurationBase
+                        .getAuthSettings();
+
+                AuthorizationHandlerLoginPassword.clearTokenCache(authSettings);
                 try {
                     dataList = execGetRecords(httpclient, magentoUrl);
                     return dataList;
-                } catch (UserTokenExpiredException e) {
-                    // try to get new token
-                    AuthenticationLoginPasswordSettings authSettings = (AuthenticationLoginPasswordSettings) magentoCmsConfigurationBase
-                            .getAuthSettings();
-
-                    AuthorizationHandlerLoginPassword.clearTokenCache(authSettings);
-                    try {
-                        dataList = execGetRecords(httpclient, magentoUrl);
-                        return dataList;
-                    } catch (UserTokenExpiredException e1) {
-                        throw new BadRequestException("User unauthorised exception");
-                    }
+                } catch (UserTokenExpiredException e1) {
+                    throw new BadRequestException("User unauthorised exception");
                 }
-            } finally {
-                httpclient.close();
             }
         }
 
@@ -92,21 +89,8 @@ public class MagentoHttpServiceFactory {
                     EntityUtils.consume(entity);
                     return dataList;
                 } else if (response.getStatusLine().getStatusCode() == 400) {
-                    int status = response.getStatusLine().getStatusCode();
-                    HttpEntity entity = response.getEntity();
-                    JsonParser jsonParser = jsonParserFactory.createParser(entity.getContent());
-                    JsonObject errorObject = jsonParser.getObject();
-                    /*
-                     * process messages like this:
-                     * {"message":"%fieldName is a required field.","parameters":{"fieldName":"searchCriteria"}}
-                     */
-                    String message = errorObject.getJsonString("message").getString();
-                    if (errorObject.getJsonObject("parameters") != null) {
-                        for (Map.Entry<String, JsonValue> parameter : errorObject.getJsonObject("parameters").entrySet()) {
-                            message = message.replaceAll("%" + parameter.getKey(), parameter.getValue().toString());
-                        }
-                    }
-                    throw new BadRequestException(message);
+                    handleBadRequest400(response, null);
+                    return null;
                 } else if (response.getStatusLine().getStatusCode() == 401
                         && magentoCmsConfigurationBase.getAuthenticationType() == AuthenticationType.LOGIN_PASSWORD) {
                     // maybe token is expired
@@ -122,26 +106,21 @@ public class MagentoHttpServiceFactory {
         public JsonObject postRecords(String magentoUrl, JsonObject dataList)
                 throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException,
                 UnknownAuthenticationTypeException, BadRequestException, BadCredentialsException {
-            CloseableHttpClient httpclient = HttpClients.createDefault();
             try {
+                JsonObject res = execPostRecords(httpclient, magentoUrl, dataList);
+                return res;
+            } catch (UserTokenExpiredException e) {
+                // try to get new token
+                AuthenticationLoginPasswordSettings authSettings = (AuthenticationLoginPasswordSettings) magentoCmsConfigurationBase
+                        .getAuthSettings();
+
+                AuthorizationHandlerLoginPassword.clearTokenCache(authSettings);
                 try {
                     JsonObject res = execPostRecords(httpclient, magentoUrl, dataList);
                     return res;
-                } catch (UserTokenExpiredException e) {
-                    // try to get new token
-                    AuthenticationLoginPasswordSettings authSettings = (AuthenticationLoginPasswordSettings) magentoCmsConfigurationBase
-                            .getAuthSettings();
-
-                    AuthorizationHandlerLoginPassword.clearTokenCache(authSettings);
-                    try {
-                        JsonObject res = execPostRecords(httpclient, magentoUrl, dataList);
-                        return res;
-                    } catch (UserTokenExpiredException e1) {
-                        throw new BadRequestException("User unauthorised exception");
-                    }
+                } catch (UserTokenExpiredException e1) {
+                    throw new BadRequestException("User unauthorised exception");
                 }
-            } finally {
-                httpclient.close();
             }
         }
 
@@ -166,26 +145,8 @@ public class MagentoHttpServiceFactory {
                     EntityUtils.consume(entity);
                     return newRecord;
                 } else if (response.getStatusLine().getStatusCode() == 400) {
-                    int status = response.getStatusLine().getStatusCode();
-                    HttpEntity entity = response.getEntity();
-                    JsonParser jsonParser = jsonParserFactory.createParser(entity.getContent());
-                    JsonObject errorObject = jsonParser.getObject();
-                    /*
-                     * process messages like this:
-                     * {"message":"%fieldName is a required field.","parameters":{"fieldName":"searchCriteria"}}
-                     */
-                    String message = errorObject.getJsonString("message").getString();
-                    if (errorObject.get("parameters").getValueType() == JsonValue.ValueType.OBJECT) {
-                        for (Map.Entry<String, JsonValue> parameter : errorObject.getJsonObject("parameters").entrySet()) {
-                            message = message.replaceAll("%" + parameter.getKey(), parameter.getValue().toString());
-                        }
-                    } else if (errorObject.get("parameters").getValueType() == JsonValue.ValueType.ARRAY) {
-                        JsonArray params = errorObject.getJsonArray("parameters");
-                        for (int i = 0; i < params.size(); i++) {
-                            message = message.replaceAll("%" + (i + 1), params.getString(i));
-                        }
-                    }
-                    throw new BadRequestException(message);
+                    handleBadRequest400(response, dataList.toString());
+                    return null;
                 } else if (response.getStatusLine().getStatusCode() == 401
                         && magentoCmsConfigurationBase.getAuthenticationType() == AuthenticationType.LOGIN_PASSWORD) {
                     // maybe token is expired
@@ -197,6 +158,33 @@ public class MagentoHttpServiceFactory {
                 response.close();
             }
         }
+    }
+
+    private void handleBadRequest400(CloseableHttpResponse response, String requestObject)
+            throws BadRequestException, IOException {
+        int status = response.getStatusLine().getStatusCode();
+        HttpEntity entity = response.getEntity();
+        JsonParser jsonParser = jsonParserFactory.createParser(entity.getContent());
+        JsonObject errorObject = jsonParser.getObject();
+        /*
+         * process messages like this:
+         * {"message":"%fieldName is a required field.","parameters":{"fieldName":"searchCriteria"}}
+         */
+        String message = errorObject.getJsonString("message").getString();
+        if (errorObject.get("parameters") != null) {
+            if (errorObject.get("parameters").getValueType() == JsonValue.ValueType.OBJECT) {
+                for (Map.Entry<String, JsonValue> parameter : errorObject.getJsonObject("parameters").entrySet()) {
+                    message = message.replaceAll("%" + parameter.getKey(), parameter.getValue().toString());
+                }
+            } else if (errorObject.get("parameters").getValueType() == JsonValue.ValueType.ARRAY) {
+                JsonArray params = errorObject.getJsonArray("parameters");
+                for (int i = 0; i < params.size(); i++) {
+                    message = message.replaceAll("%" + (i + 1), params.getString(i));
+                }
+            }
+        }
+        throw new BadRequestException(
+                "An error occurred: " + message + (requestObject == null ? "" : "For object: " + requestObject));
     }
 
     public MagentoHttpService createMagentoHttpService(MagentoCmsConfigurationBase magentoCmsConfigurationBase) {
