abdd4f764e67793c85846ae301c3c0c1913d8a1b
Maksym Basiuk <mbasiuk@talend.com>
2018-08-29 17:50:24 +0300

    feat(TDI-40709): add default values, control visibility of search prop

--- netsuite/src/main/java/org/talend/components/netsuite/dataset/SearchConditionConfiguration.java
+++ netsuite/src/main/java/org/talend/components/netsuite/dataset/SearchConditionConfiguration.java
@@ -4,15 +4,13 @@ import java.io.Serializable;
 
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.configuration.action.Suggestable;
+import org.talend.sdk.component.api.configuration.condition.ActiveIf;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.meta.Documentation;
 
 import lombok.Data;
 
-@GridLayout({
-        // the generated layout put one configuration entry per line,
-        // customize it as much as needed
-        @GridLayout.Row({ "field" }), @GridLayout.Row({ "operator" }), @GridLayout.Row({ "value" }),
+@GridLayout({ @GridLayout.Row({ "field" }), @GridLayout.Row({ "operator" }), @GridLayout.Row({ "value" }),
         @GridLayout.Row({ "value2" }) })
 @Documentation("TODO fill the documentation for this configuration")
 @Data
@@ -29,10 +27,15 @@ public class SearchConditionConfiguration implements Serializable {
     private String operator;
 
     @Option
+    @ActiveIf(target = "operator", negate = true, value = { "String.empty", "String.notEmpty", "Long.empty", "Long.notEmpty",
+            "Double.empty", "Double.notEmpty", "Date.empty", "Date.notEmpty", "TextNumber.empty", "TextNumber.notEmpty" })
     @Documentation("TODO fill the documentation for this parameter")
     private String value;
 
     @Option
+    @ActiveIf(target = "operator", value = { "Long.between", "Long.notBetween", "Double.between", "Double.notBetween",
+            "Date.within", "Date.notWithin", "TextNumber.between", "TextNumber.notBetween" })
     @Documentation("TODO fill the documentation for this parameter")
     private String value2;
+
 }
