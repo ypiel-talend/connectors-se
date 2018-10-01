39fc7b8ecf567d585f18bd1667e5adfbcf123ebd
Anas KHABALI <akhabali@talend.com>
2018-10-01 15:45:14 +0100

    TDI-40707 : close jdbc connection in datastore healthcheck

--- src/main/java/org/talend/components/jdbc/service/ActionService.java
+++ src/main/java/org/talend/components/jdbc/service/ActionService.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -67,8 +67,7 @@ public class ActionService {
 
     @HealthCheck(ACTION_BASIC_HEALTH_CHECK)
     public HealthCheckStatus validateBasicDatastore(@Option final BasicDatastore datastore) {
-        try {
-            this.jdbcDriversService.connection(datastore);
+        try (final Connection ignored = this.jdbcDriversService.connection(datastore)) {
         } catch (final Exception e) {
             return new HealthCheckStatus(HealthCheckStatus.Status.KO, e.getMessage());
         }
