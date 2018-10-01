bfb473b795bac131da00888ca6180accfe0a70cb
Remy Dubois <rdubois@talend.com>
2018-10-01 15:25:14 +0200

    Fix the JUNIT

--- localio/src/test/java/org/talend/components/localio/devnull/DevNullOutputTest.java
+++ localio/src/test/java/org/talend/components/localio/devnull/DevNullOutputTest.java
@@ -1,21 +1,24 @@
 package org.talend.components.localio.devnull;
 
-import org.apache.avro.generic.IndexedRecord;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.transforms.Create;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
 import org.junit.jupiter.api.Test;
 import org.talend.daikon.avro.GenericDataRecordHelper;
-import org.talend.sdk.component.api.service.Service;
+import org.talend.sdk.component.api.record.Record;
 import org.talend.sdk.component.junit.ComponentsHandler;
 import org.talend.sdk.component.junit5.Injected;
 import org.talend.sdk.component.junit5.WithComponents;
-
-import javax.json.JsonBuilderFactory;
+import org.talend.sdk.component.runtime.beam.TalendIO;
+import org.talend.sdk.component.runtime.beam.coder.registry.SchemaRegistryCoder;
+import org.talend.sdk.component.runtime.beam.spi.record.AvroRecord;
+import org.talend.sdk.component.runtime.beam.transform.RecordNormalizer;
+import org.talend.sdk.component.runtime.output.Processor;
+import java.util.Optional;
 
 import static java.util.Collections.emptyMap;
-import static org.talend.sdk.component.runtime.manager.ComponentManager.ComponentType.PROCESSOR;
 
 @WithComponents("org.talend.components.localio")
 class DevNullOutputTest {
@@ -25,15 +28,17 @@ class DevNullOutputTest {
 
     @Test
     void ensureItRuns() {
-        // TODO: Rewrite the test to handle the new processor implementation
-        /*
         final Pipeline pipeline = Pipeline.create();
-        final PTransform<PCollection<IndexedRecord>, PCollection<IndexedRecord>> input = handler.asManager()
-                .createComponent("LocalIO", "DevNullOutputRuntime", PROCESSOR, 1, emptyMap())
-                .map(e -> (PTransform<PCollection<IndexedRecord>, PCollection<IndexedRecord>>) e)
+        final Optional<Processor> processor = handler.asManager()
+                .findProcessor("LocalIO", "DevNullOutputRuntime", 1, emptyMap());
+
+        final PTransform<PCollection<Record>, PDone> input = processor
+                .map(TalendIO::write)
                 .orElseThrow(() -> new IllegalArgumentException("No component for fixed flow input"));
-        pipeline.apply(Create.of(GenericDataRecordHelper.createRecord(new Object[] { "a", 1 }))).apply(input);
+
+        pipeline.apply(Create.of((Record) new AvroRecord(GenericDataRecordHelper.createRecord(new Object[]{"a", 1}))).withCoder(SchemaRegistryCoder.of()))
+                .apply("Normalizer", RecordNormalizer.of(processor.get().plugin()))
+                .apply(input);
         pipeline.run().waitUntilFinish();
-        */
     }
 }
