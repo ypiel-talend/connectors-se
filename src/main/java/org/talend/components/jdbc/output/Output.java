c0cfcb94d5fa8a595ff7fd3998e0f0e77c9f0450
Anas KHABALI <akhabali@talend.com>
2018-10-02 14:03:52 +0100

    handle missing fields, null values and add more tests

--- src/main/java/org/talend/components/jdbc/output/Output.java
+++ src/main/java/org/talend/components/jdbc/output/Output.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -14,7 +14,6 @@ package org.talend.components.jdbc.output;
 
 import java.io.Serializable;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import javax.annotation.PostConstruct;
@@ -50,10 +49,6 @@ public class Output implements Serializable {
 
     private final I18nMessage i18n;
 
-    private Connection connection;
-
-    private PreparedStatement preparedStatement = null;
-
     private StatementManager statementManager;
 
     public Output(@Option("configuration") final OutputDataset dataset, final JdbcService jdbcDriversService,
@@ -65,106 +60,35 @@ public class Output implements Serializable {
 
     @PostConstruct
     public void init() {
-        connection = jdbcDriversService.connection(dataset.getConnection());
+        final Connection connection = jdbcDriversService.connection(dataset.getConnection());
         try {
             connection.setAutoCommit(false);
         } catch (SQLException e) {
             log.error("Can't deactivate auto-commit, this may alter the performance if this batch");
         }
 
-        this.statementManager = StatementManager.get(dataset);
+        this.statementManager = StatementManager.get(dataset, connection, i18n);
     }
 
     @BeforeGroup
     public void beforeGroup() {
-        if (preparedStatement != null) {
-            try {
-                preparedStatement.clearParameters();
-                preparedStatement.clearBatch();
-            } catch (SQLException e) {
-                throw new IllegalStateException("Can't clear prepared statement.");
-            }
-        }
+        this.statementManager.clear();
     }
 
     @ElementListener
     public void elementListener(@Input final Record record) {
-        try {
-            if (preparedStatement == null) {
-                switch (dataset.getActionOnData()) {
-                default:
-                case INSERT:
-                    preparedStatement = connection.prepareStatement(statementManager.createQuery(record));
-                    break;
-                case DELETE:
-                    preparedStatement = connection.prepareStatement(statementManager.createQuery(record));
-                    break;
-                case UPDATE:
-                    preparedStatement = connection.prepareStatement(statementManager.createQuery(record));
-                    break;
-                }
-            }
-
-            try {
-                statementManager.populateParameters(preparedStatement, record);
-                preparedStatement.addBatch();
-            } catch (SQLException e) {
-                // todo : how to handle record errors ? => ignored for now
-                log.error("error with record " + record.toString() + "\nThe record was ignored.", e);
-            }
-
-        } catch (final SQLException e) {
-            throw new IllegalStateException("can't create the prepared statement", e);
-        }
-
+        statementManager.addBatch(record);
     }
 
     @AfterGroup
     public void afterGroup() {
-        try {
-            final int[] result = preparedStatement.executeBatch();
-            if (!connection.getAutoCommit()) {
-                connection.commit();
-            }
-        } catch (SQLException e) {
-            // rollback the group
-            try {
-                connection.rollback();
-            } catch (SQLException e1) {
-                log.error("Can't rollback statements", e);
-            }
-
-            // fixme : should we transform this component to a processor and :
-            // 2 . emit rejected records
-
-            StringBuilder batchErrorMessage = new StringBuilder();
-            SQLException batchError = e;
-            while (batchError.getNextException() != null) {
-                batchErrorMessage.append("- ").append(batchError.getNextException().getLocalizedMessage()).append("\n");
-                batchError = batchError.getNextException();
-            }
-            throw new IllegalStateException(batchErrorMessage.toString(), e);
-        }
+        statementManager.executeBatch();
     }
 
     @PreDestroy
     public void preDestroy() {
-        if (preparedStatement != null) {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                log.warn("Can't close the prepared statement properly.", e);
-            }
-        }
-
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                log.warn("Can't close the connection properly.", e);
-            } finally {
-                connection = null;
-            }
+        if (statementManager != null) {
+            statementManager.close();
         }
     }
 
--- src/main/java/org/talend/components/jdbc/output/internal/DeleteStatementManager.java
+++ src/main/java/org/talend/components/jdbc/output/internal/DeleteStatementManager.java
@@ -1,59 +1,65 @@
 /*
  * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
 package org.talend.components.jdbc.output.internal;
 
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toMap;
 
+import java.sql.Connection;
 import java.util.AbstractMap;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.talend.components.jdbc.dataset.OutputDataset;
+import org.talend.components.jdbc.service.I18nMessage;
 import org.talend.sdk.component.api.record.Record;
+import org.talend.sdk.component.api.record.Schema;
 
 public class DeleteStatementManager extends StatementManager {
 
-    private final String tableName;
+    private final OutputDataset dataset;
 
     private final String[] deleteKeys;
 
-    private final Map<String, Integer> indexedColumns;
-
-    DeleteStatementManager(final OutputDataset dataset) {
-        super();
-        tableName = dataset.getTableName();
-        deleteKeys = dataset.getDeleteKeys().stream().filter(Objects::nonNull).filter(key -> !key.isEmpty()).map(String::trim)
-                .toArray(String[]::new);
-        if (deleteKeys.length == 0) {
-            throw new IllegalStateException("Please select the delete keys");
+    DeleteStatementManager(final OutputDataset dataset, final Connection connection, final I18nMessage i18nMessage) {
+        super(connection, i18nMessage);
+        this.dataset = dataset;
+        this.deleteKeys = ofNullable(dataset.getDeleteKeys()).orElse(emptyList()).stream().filter(Objects::nonNull)
+                .filter(key -> !key.isEmpty()).map(String::trim).toArray(String[]::new);
+        if (this.deleteKeys.length == 0) {
+            throw new IllegalStateException(this.i18n.errorNoKeyForDeleteQuery());
         }
 
-        indexedColumns = IntStream.rangeClosed(1, deleteKeys.length)
-                .mapToObj(i -> new AbstractMap.SimpleEntry<>(deleteKeys[i - 1], i))
-                .collect(toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
     }
 
     @Override
     public String createQuery(final Record record) {
-        return "DELETE FROM " + tableName + " WHERE " + Stream.of(deleteKeys).map(c -> c + " = ?").collect(joining(" AND "));
+        return "DELETE FROM " + dataset.getTableName() + " WHERE "
+                + Stream.of(deleteKeys).map(c -> c + " = ?").collect(joining(" AND "));
     }
 
     @Override
-    public Map<String, Integer> getIndexedColumns() {
-        return indexedColumns;
+    public Map<Schema.Entry, Integer> getSqlQueryParams(final Record record) {
+        return IntStream.rangeClosed(1, deleteKeys.length)
+                .mapToObj(i -> new AbstractMap.SimpleEntry<>(
+                        record.getSchema().getEntries().stream().filter(e -> e.getName().equals(deleteKeys[i - 1])).findFirst()
+                                .orElseThrow(() -> new IllegalStateException(i18n.errorNoFieldForQueryParam(deleteKeys[i - 1]))),
+                        i))
+                .collect(toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue));
     }
-
 }
