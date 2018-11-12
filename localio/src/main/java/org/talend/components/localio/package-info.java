3bed52dc43eadf2f449ed48975dde226fe9021f0
Pierre Bailly-Ferry <pbailly@talend.com>
2018-11-12 11:53:48 +0100

    feat(TFD-4616): Remove processor-beam and localio. (#57)
    * feat(TFD-4616): Remove processor-beam.
    
    * feat(TFD-4616): Remove localio.

--- localio/src/main/java/org/talend/components/localio/package-info.java
+++
@@ -1,8 +0,0 @@
-@Icon(FLOW_O)
-@Components(family = "LocalIO")
-package org.talend.components.localio;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
-
-import static org.talend.sdk.component.api.component.Icon.IconType.FLOW_O;
--- processing-beam/src/main/java/org/talend/components/processing/package-info.java
+++
@@ -1,6 +0,0 @@
-@Icon(Icon.IconType.FILTER)
-@Components(family = "Processing")
-package org.talend.components.processing;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
