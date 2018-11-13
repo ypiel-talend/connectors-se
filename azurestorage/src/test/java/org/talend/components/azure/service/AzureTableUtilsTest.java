515adc92e63bf738252f6f3faf256c65e74ee561
Dmytro Sylaiev <dsylaiev@talend.com>
2018-11-13 17:33:37 +0200

    fix(TDI-40715): Fix datastore message

--- azurestorage/src/test/java/org/talend/components/azure/service/AzureTableUtilsTest.java
+++ azurestorage/src/test/java/org/talend/components/azure/service/AzureTableUtilsTest.java
@@ -12,9 +12,16 @@
  */
 package org.talend.components.azure.service;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.talend.components.azure.common.Comparison;
+import org.talend.components.azure.table.input.InputProperties;
 
 import com.microsoft.azure.storage.OperationContext;
 
@@ -34,5 +41,26 @@ public class AzureTableUtilsTest {
         OperationContext contextFirst = AzureConnectionService.getTalendOperationContext();
 
         assertEquals(contextFirst, AzureConnectionService.getTalendOperationContext());
+    }
+
+    @Test
+    public void testGenerateFilterExpression() {
+        String columnName = "someColumn";
+        InputProperties configuration = new InputProperties();
+        InputProperties.FilterExpression expression1 = new InputProperties.FilterExpression();
+        expression1.setColumn(columnName);
+        expression1.setFieldType(InputProperties.FieldType.NUMERIC);
+        expression1.setFunction(Comparison.GREATER_THAN);
+        expression1.setValue("2");
+        List<InputProperties.FilterExpression> filterExpressions = new ArrayList<>();
+        filterExpressions.add(expression1);
+        configuration.setFilterExpressions(filterExpressions);
+
+        String resultFilterExpression = AzureTableUtils.generateCombinedFilterConditions(configuration);
+
+        assertFalse(resultFilterExpression.isEmpty());
+        assertTrue(resultFilterExpression.contains(columnName));
+        assertTrue(resultFilterExpression.contains("gt"));
+        assertTrue(resultFilterExpression.contains("2"));
     }
 }
