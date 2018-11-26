2e61191f99c911de451a879bea2ff5b1a016fa26
s.bovsunovskyi <s.bovsunovskyi@globallogic.com>
2018-11-26 18:59:08 +0200

    metadata icon

--- magentocms/src/main/java/org/talend/components/magentocms/package-info.java
+++ magentocms/src/main/java/org/talend/components/magentocms/package-info.java
@@ -1,5 +1,5 @@
 @Components(family = "Magento", categories = "Business")
-@Icon(value = Icon.IconType.CUSTOM, custom = "MagentoComponents")
+@Icon(value = Icon.IconType.CUSTOM, custom = "magento_metadata")
 package org.talend.components.magentocms;
 
 import org.talend.sdk.component.api.component.Components;
