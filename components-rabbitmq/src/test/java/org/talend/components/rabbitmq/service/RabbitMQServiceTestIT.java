464283590b1cd7131de128ae38564301fab98f3e
vizotenko <vizotenko@talend.com>
2019-02-05 07:44:15 +0200

    feat(TDI-41172): use HTTP API to check subscription

--- components-rabbitmq/src/test/java/org/talend/components/rabbitmq/service/RabbitMQServiceTestIT.java
+++ components-rabbitmq/src/test/java/org/talend/components/rabbitmq/service/RabbitMQServiceTestIT.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.http.client.Client;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -16,7 +17,6 @@ import org.talend.components.rabbitmq.datastore.RabbitMQDataStore;
 import org.talend.components.rabbitmq.output.OutputConfiguration;
 import org.talend.components.rabbitmq.source.InputMapperConfiguration;
 import org.talend.sdk.component.api.service.Service;
-import org.talend.sdk.component.api.service.healthcheck.HealthCheckStatus;
 import org.talend.sdk.component.junit.BaseComponentsHandler;
 import org.talend.sdk.component.junit5.Injected;
 import org.talend.sdk.component.junit5.WithComponents;
@@ -26,23 +26,25 @@ import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.talend.components.rabbitmq.MessageConst.MESSAGE_CONTENT;
+import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.PASSWORD;
 import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.TEST_MESSAGE;
 import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.FANOUT_EXCHANGE_NAME;
 import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.DIRECT_EXCHANGE_NAME;
 import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.QUEUE_NAME;
 import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.MAXIMUM_MESSAGES;
+import static org.talend.components.rabbitmq.testutils.RabbitMQTestConstants.USER_NAME;
 import static org.talend.sdk.component.junit.SimpleFactory.configurationByExample;
 
 @WithComponents("org.talend.components.rabbitmq") // component package
@@ -63,6 +65,8 @@ public class RabbitMQServiceTestIT {
     private ActionService actionService;
 
     private RabbitMQTestExtention.TestContext testContext;
+
+    private Client client;
 
     @BeforeAll
     private void init(RabbitMQTestExtention.TestContext testContext) {
@@ -91,18 +95,19 @@ public class RabbitMQServiceTestIT {
     }
 
     @Test
-    public void receiveFanoutMessage() {
+    public void receiveFanoutMessage() throws MalformedURLException, URISyntaxException {
         OutputConfiguration outputConfiguration = getOutputConfiguration();
         outputConfiguration.getBasicConfig().setReceiverType(ReceiverType.EXCHANGE);
-        CountDownLatch count = new CountDownLatch(1);
-        Thread thread = new Thread(() -> {
-            try {
-                count.await(30, TimeUnit.SECONDS);
-                sendMessageToExchange(outputConfiguration.getBasicConfig().getConnection(), BuiltinExchangeType.FANOUT,
-                        FANOUT_EXCHANGE_NAME);
+        client = new Client("http://" + testContext.getDataStore().getHostname() + ":" + testContext.getHttpPort() + "/api",
+                USER_NAME, PASSWORD);
 
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Thread thread = new Thread(() -> {
+            while (true) {
+                if (isConnected()) {
+                    sendMessageToExchange(outputConfiguration.getBasicConfig().getConnection(), BuiltinExchangeType.FANOUT,
+                            FANOUT_EXCHANGE_NAME);
+                    break;
+                }
             }
         });
         thread.start();
@@ -127,18 +132,19 @@ public class RabbitMQServiceTestIT {
     }
 
     @Test
-    public void receiveDirectMessage() {
+    public void receiveDirectMessage() throws MalformedURLException, URISyntaxException {
         OutputConfiguration outputConfiguration = getOutputConfiguration();
         outputConfiguration.getBasicConfig().setReceiverType(ReceiverType.EXCHANGE);
-        CountDownLatch count = new CountDownLatch(1);
-        Thread thread = new Thread(() -> {
-            try {
-                count.await(30, TimeUnit.SECONDS);
-                sendMessageToExchange(outputConfiguration.getBasicConfig().getConnection(), BuiltinExchangeType.DIRECT,
-                        DIRECT_EXCHANGE_NAME);
+        client = new Client("http://" + testContext.getDataStore().getHostname() + ":" + testContext.getHttpPort() + "/api",
+                USER_NAME, PASSWORD);
 
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        Thread thread = new Thread(() -> {
+            while (true) {
+                if (isConnected()) {
+                    sendMessageToExchange(outputConfiguration.getBasicConfig().getConnection(), BuiltinExchangeType.DIRECT,
+                            DIRECT_EXCHANGE_NAME);
+                    break;
+                }
             }
         });
         thread.start();
@@ -160,13 +166,6 @@ public class RabbitMQServiceTestIT {
 
         assertEquals(TEST_MESSAGE, ((JsonObject) optional.get()).getString((MESSAGE_CONTENT)),
                 "Sent and received messages should be equal");
-    }
-
-    @Test
-    public void testSuccessfulConnection() {
-        HealthCheckStatus status = actionService.validateBasicDatastore(testContext.getDataStore());
-
-        assertEquals(HealthCheckStatus.Status.OK, status.getStatus());
     }
 
     private void sendMessageToExchange(RabbitMQDataStore store, BuiltinExchangeType exchangeType, String exchangeName) {
@@ -207,6 +206,11 @@ public class RabbitMQServiceTestIT {
         configuration.setBasicConfig(basicConfiguration);
         configuration.setMaximumMessages(MAXIMUM_MESSAGES);
         return configuration;
+    }
+
+    private boolean isConnected() {
+        return !client.getConnections().isEmpty()
+                && client.getConnection(client.getConnections().get(0).getName()).getChannels() > 0;
     }
 
 }
