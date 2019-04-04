be88507332679b37b85fa27ce09d8580dee34587
s.bovsunovskyi <s.bovsunovskyi@globallogic.com>
2019-04-04 17:41:46 +0300

    feat(TDI-41172): rabbitMQ. fix test blocking

--- components-rabbitmq/src/main/java/org/talend/components/rabbitmq/service/RabbitMQService.java
+++ components-rabbitmq/src/main/java/org/talend/components/rabbitmq/service/RabbitMQService.java
@@ -12,19 +12,19 @@
 // ============================================================================
 package org.talend.components.rabbitmq.service;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.TimeoutException;
-
+import com.rabbitmq.client.BlockedListener;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.talend.components.rabbitmq.datastore.RabbitMQDataStore;
 import org.talend.components.rabbitmq.exception.CreateChannelException;
 import org.talend.sdk.component.api.service.Service;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @Service
@@ -42,23 +42,43 @@ public class RabbitMQService {
         if (store.getTLS()) {
             factory.useSslProtocol(SSLContext.getDefault());
         }
-        return factory.newConnection();
+        Connection conn = factory.newConnection();
+        conn.addBlockedListener(new BlockedListener() {
+
+            @Override
+            public void handleBlocked(String s) throws IOException {
+                System.out.println("The connection is Blocked");
+            }
+
+            @Override
+            public void handleUnblocked() throws IOException {
+                System.out.println("The connection is UnBlocked");
+            }
+        });
+        log.debug("create connection to rabbit: " + ":" + conn);
+        return conn;
     }
 
     public Channel createChannel(Connection connection) {
+        log.debug("create channel on connection" + ":" + connection);
+
         Channel channel;
         try {
             channel = connection.createChannel();
         } catch (IOException e) {
             throw new CreateChannelException(i18n.errorCantCreateChannel(), e);
         }
+        log.debug("create channel on connection finished" + ":" + connection + ":" + channel);
         return channel;
     }
 
     public void closeConnection(Connection connection) {
+        log.debug("closing connection" + ":" + connection);
         if (connection != null) {
             try {
                 connection.close();
+                System.out.println("closing connection finish" + ":" + connection);
+
             } catch (IOException e) {
                 log.warn(i18n.warnConnectionCantBeClosed(), e);
             }
