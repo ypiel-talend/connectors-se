0722459a5959b39978000b3f68136fb926ee3d83
s.bovsunovskyi <s.bovsunovskyi@globallogic.com>
2019-04-05 12:21:29 +0300

    feat(TDI-40709): add discover schema function  Conflicts: 	netsuite/src/main/java/org/talend/components/netsuite/dataset/NetsuiteInputDataSet.java 	netsuite/src/main/java/org/talend/components/netsuite/runtime/NetSuiteDatasetRuntime.java 	netsuite/src/main/java/org/talend/components/netsuite/runtime/NetSuiteDatasetRuntimeImpl.java 	netsuite/src/main/java/org/talend/components/netsuite/service/NetsuiteService.java 	netsuite/src/main/java/org/talend/components/netsuite/service/UIActionService.java 	netsuite/src/main/resources/org/talend/components/netsuite/dataset/Messages.properties

--- netsuite/src/main/java/org/talend/components/netsuite/datastore/NetsuiteDataStore.java
+++ netsuite/src/main/java/org/talend/components/netsuite/datastore/NetsuiteDataStore.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.configuration.action.Checkable;
 import org.talend.sdk.component.api.configuration.type.DataStore;
-import org.talend.sdk.component.api.configuration.ui.DefaultValue;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayouts;
 import org.talend.sdk.component.api.configuration.ui.widget.Credential;
