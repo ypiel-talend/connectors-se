540a49c70c5db079cb7f303ae4d481a6b2bb280e
s.bovsunovskyi <s.bovsunovskyi@globallogic.com>
2019-04-05 12:26:55 +0300

    feat(TDI-40709): fix load record types and guess schema
     Conflicts:
    	netsuite/src/main/java/org/talend/components/netsuite/dataset/NetsuiteInputDataSet.java
    	netsuite/src/main/java/org/talend/components/netsuite/dataset/NetsuiteOutputDataSet.java
    	netsuite/src/main/java/org/talend/components/netsuite/dataset/SearchConditionConfiguration.java
    	netsuite/src/main/java/org/talend/components/netsuite/processor/NetsuiteOutputProcessor.java
    	netsuite/src/main/java/org/talend/components/netsuite/runtime/NetSuiteEndpoint.java
    	netsuite/src/main/java/org/talend/components/netsuite/runtime/v2018_1/client/NetSuiteClientServiceImpl.java
    	netsuite/src/main/java/org/talend/components/netsuite/service/NetsuiteService.java
    	netsuite/src/main/java/org/talend/components/netsuite/service/UIActionService.java
    	netsuite/src/main/java/org/talend/components/netsuite/source/NetsuiteInputSource.java
    	netsuite/src/main/resources/org/talend/components/netsuite/Messages.properties
    	netsuite/src/main/resources/org/talend/components/netsuite/dataset/Messages.properties
    	netsuite/src/main/resources/org/talend/components/netsuite/datastore/Messages.properties
    	netsuite/src/test/java/org/talend/components/netsuite/processor/NetsuiteOutputProcessorTest.java

--- netsuite/src/main/java/org/talend/components/netsuite/dataset/NetSuiteCommonDataSet.java
+++ netsuite/src/main/java/org/talend/components/netsuite/dataset/NetSuiteCommonDataSet.java
@@ -1,15 +1,14 @@
 package org.talend.components.netsuite.dataset;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.talend.components.netsuite.datastore.NetsuiteDataStore;
 import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.configuration.action.Suggestable;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
 import org.talend.sdk.component.api.configuration.ui.layout.GridLayouts;
 import org.talend.sdk.component.api.meta.Documentation;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
