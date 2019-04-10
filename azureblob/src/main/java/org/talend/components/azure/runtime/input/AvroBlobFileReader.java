845684afbe0be898f3d6ee0c5c10970507467909
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2019-04-10 14:32:39 +0300

    chore(TDI-41796): Fix copyright year

--- azureblob/src/main/java/org/talend/components/azure/runtime/input/AvroBlobFileReader.java
+++ azureblob/src/main/java/org/talend/components/azure/runtime/input/AvroBlobFileReader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+ * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -24,6 +24,6 @@ public class AvroBlobFileReader extends BlobFileReader {
 
     @Override
     public Record readRecord() {
-        return null;
+       return null;
     }
 }
--- azureblob/src/main/java/org/talend/components/azure/service/MessageService.java
+++ azureblob/src/main/java/org/talend/components/azure/service/MessageService.java
@@ -10,6 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
+
 package org.talend.components.azure.service;
 
 import org.talend.sdk.component.api.internationalization.Internationalized;
