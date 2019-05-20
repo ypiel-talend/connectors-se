9a65dafc2ed5087cf4c9860b8a1aa1771d818f2c
voievidko <roman.voievidko@synapse.com>
2019-05-20 15:46:24 +0300

    feat(TDI-40639): uncomment tests

--- couchbase/src/test/java/org/talend/components/couchbase/testutils/CouchbaseServiceTest.java
+++ couchbase/src/test/java/org/talend/components/couchbase/testutils/CouchbaseServiceTest.java
@@ -28,55 +28,54 @@ import static org.junit.jupiter.api.Assertions.*;
 @WithComponents("org.talend.components.couchbase")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("Testing of CouchbaseService class")
-// public class CouchbaseServiceTest extends CouchbaseUtilTest {
-public class CouchbaseServiceTest {
+public class CouchbaseServiceTest extends CouchbaseUtilTest {
 
     @Service
     private CouchbaseService couchbaseService;
-    //
-    // @Test
-    // @DisplayName("Test successful connection")
-    // void couchbaseSuccessfulConnectionTest() {
-    // CouchbaseDataStore couchbaseDataStore = new CouchbaseDataStore();
-    // couchbaseDataStore.setBootstrapNodes(COUCHBASE_CONTAINER.getContainerIpAddress());
-    // couchbaseDataStore.setBucket(BUCKET_NAME);
-    // couchbaseDataStore.setPassword(BUCKET_PASSWORD);
-    // couchbaseDataStore.setConnectTimeout(DEFAULT_TIMEOUT_IN_SEC);
-    //
-    // assertEquals(HealthCheckStatus.Status.OK, couchbaseService.healthCheck(couchbaseDataStore).getStatus());
-    // }
-    //
-    // @Test
-    // @DisplayName("Test unsuccessful connection")
-    // void couchbaseNotSuccessfulConnectionTest() {
-    // String wrongPassword = "wrongpass";
-    //
-    // CouchbaseDataStore couchbaseDataStore = new CouchbaseDataStore();
-    // couchbaseDataStore.setBootstrapNodes(COUCHBASE_CONTAINER.getContainerIpAddress());
-    // couchbaseDataStore.setBucket(BUCKET_NAME);
-    // couchbaseDataStore.setPassword(wrongPassword);
-    // couchbaseDataStore.setConnectTimeout(DEFAULT_TIMEOUT_IN_SEC);
-    //
-    // assertEquals(HealthCheckStatus.Status.KO, couchbaseService.healthCheck(couchbaseDataStore).getStatus());
-    // }
-    //
-    // @Test
-    // @DisplayName("Two bootstrap nodes without spaces")
-    // void resolveAddressesTest() {
-    // String inputUrl = "192.168.0.1,192.168.0.2";
-    // String[] resultArrayWithUrls = couchbaseService.resolveAddresses(inputUrl);
-    // assertEquals("192.168.0.1", resultArrayWithUrls[0], "first expected node");
-    // assertEquals("192.168.0.2", resultArrayWithUrls[1], "second expected node");
-    // }
-    //
-    // @Test
-    // @DisplayName("Two bootstrap nodes with extra spaces")
-    // void resolveAddressesWithSpacesTest() {
-    // String inputUrl = " 192.168.0.1, 192.168.0.2";
-    // String[] resultArrayWithUrls = couchbaseService.resolveAddresses(inputUrl);
-    // assertEquals("192.168.0.1", resultArrayWithUrls[0], "first expected node");
-    // assertEquals("192.168.0.2", resultArrayWithUrls[1], "second expected node");
-    // }
+
+    @Test
+    @DisplayName("Test successful connection")
+    void couchbaseSuccessfulConnectionTest() {
+        CouchbaseDataStore couchbaseDataStore = new CouchbaseDataStore();
+        couchbaseDataStore.setBootstrapNodes(COUCHBASE_CONTAINER.getContainerIpAddress());
+        couchbaseDataStore.setBucket(BUCKET_NAME);
+        couchbaseDataStore.setPassword(BUCKET_PASSWORD);
+        couchbaseDataStore.setConnectTimeout(DEFAULT_TIMEOUT_IN_SEC);
+
+        assertEquals(HealthCheckStatus.Status.OK, couchbaseService.healthCheck(couchbaseDataStore).getStatus());
+    }
+
+    @Test
+    @DisplayName("Test unsuccessful connection")
+    void couchbaseNotSuccessfulConnectionTest() {
+        String wrongPassword = "wrongpass";
+
+        CouchbaseDataStore couchbaseDataStore = new CouchbaseDataStore();
+        couchbaseDataStore.setBootstrapNodes(COUCHBASE_CONTAINER.getContainerIpAddress());
+        couchbaseDataStore.setBucket(BUCKET_NAME);
+        couchbaseDataStore.setPassword(wrongPassword);
+        couchbaseDataStore.setConnectTimeout(DEFAULT_TIMEOUT_IN_SEC);
+
+        assertEquals(HealthCheckStatus.Status.KO, couchbaseService.healthCheck(couchbaseDataStore).getStatus());
+    }
+
+    @Test
+    @DisplayName("Two bootstrap nodes without spaces")
+    void resolveAddressesTest() {
+        String inputUrl = "192.168.0.1,192.168.0.2";
+        String[] resultArrayWithUrls = couchbaseService.resolveAddresses(inputUrl);
+        assertEquals("192.168.0.1", resultArrayWithUrls[0], "first expected node");
+        assertEquals("192.168.0.2", resultArrayWithUrls[1], "second expected node");
+    }
+
+    @Test
+    @DisplayName("Two bootstrap nodes with extra spaces")
+    void resolveAddressesWithSpacesTest() {
+        String inputUrl = " 192.168.0.1, 192.168.0.2";
+        String[] resultArrayWithUrls = couchbaseService.resolveAddresses(inputUrl);
+        assertEquals("192.168.0.1", resultArrayWithUrls[0], "first expected node");
+        assertEquals("192.168.0.2", resultArrayWithUrls[1], "second expected node");
+    }
 
     @Test
     @DisplayName("Define if query use select *")
@@ -87,5 +86,4 @@ public class CouchbaseServiceTest {
         assertFalse(couchbaseService.isResultNeedWrapper("SELECT COUNT(*) AS count FROM `test`"));
         assertFalse(couchbaseService.isResultNeedWrapper("SELECT test.* FROM `test` limit 10"));
     }
-
 }
