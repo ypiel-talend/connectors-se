a76bd88fe17581383669a689f05084efa3094fed
s.bovsunovskyi <s.bovsunovskyi@globallogic.com>
2019-05-21 16:58:14 +0300

    test onedrive-integration-user

--- onedrive/src/test/java/org/talend/components/onedrive/OneDriveTestExtension.java
+++ onedrive/src/test/java/org/talend/components/onedrive/OneDriveTestExtension.java
@@ -15,6 +15,7 @@ import org.talend.sdk.component.maven.Server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Map;
 import java.util.Properties;
 
 @Slf4j
@@ -31,8 +32,12 @@ public class OneDriveTestExtension implements BeforeAllCallback, ParameterResolv
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws IOException {
         log.info("extension before all start");
-        log.info("NEXUS_USER" + System.getenv("NEXUS_USER") + System.getProperty("NEXUS_USER"));
-        log.info("onedrive-integration-user:" + System.getenv("onedrive-integration-user")+ System.getProperty("onedrive-integration-user"));
+        log.info("NEXUS_USER" + System.getenv("NEXUS_USER"));
+        log.info("onedrive-integration-user:" + System.getenv("onedrive-integration-user"));
+
+        for (Map.Entry<String, String> s: System.getenv().entrySet()) {
+            log.info(s.getKey() + ":" + s.getValue());
+        }
 
         readPropertiesFile();
 
