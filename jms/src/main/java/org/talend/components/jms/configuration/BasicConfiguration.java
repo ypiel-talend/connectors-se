f1b136e4cb326d681b43a1127951a1181182ea8c
vizotenko <vizotenko@talend.com>
2019-06-06 11:26:59 +0300

    feat(TDI-40737): rename jms artifactId

---
+++ jms/src/main/java/org/talend/components/jms/configuration/BasicConfiguration.java
@@ -0,0 +1,52 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.jms.configuration;
+
+import lombok.Data;
+import org.talend.components.jms.datastore.JmsDataStore;
+import org.talend.components.jms.source.DurableSubscriptionConfiguration;
+import org.talend.sdk.component.api.configuration.Option;
+import org.talend.sdk.component.api.configuration.condition.ActiveIf;
+import org.talend.sdk.component.api.configuration.constraint.Required;
+import org.talend.sdk.component.api.configuration.type.DataSet;
+import org.talend.sdk.component.api.configuration.ui.layout.GridLayout;
+import org.talend.sdk.component.api.meta.Documentation;
+
+import java.io.Serializable;
+
+
+@DataSet("JMSDataSet")
+@Data
+@GridLayout(value = { @GridLayout.Row({ "connection" }), @GridLayout.Row({ "messageType" }), @GridLayout.Row({ "destination" }),
+        @GridLayout.Row({ "subscriptionConfig" }) }, names = GridLayout.FormType.MAIN)
+public class BasicConfiguration implements Serializable {
+
+    @Option
+    @Documentation("JMS connection information")
+    private JmsDataStore connection;
+
+    @Option
+    @Documentation("Drop down list for Message Type")
+    private MessageType messageType = MessageType.TOPIC;
+
+    @Option
+    @Required
+    @Documentation("Input for TOPIC/QUEUE Name")
+    private String destination;
+
+    @Option
+    @Documentation("Durable subscription configuration")
+    @ActiveIf(target = "messageType", value = "TOPIC")
+    private DurableSubscriptionConfiguration subscriptionConfig;
+
+}
