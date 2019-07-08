d94cd60dd8d27fb63e0361dc69b2f1ae9fa45a78
Dmytro Sylaiev <dmytro.sylaiev@globallogic.com>
2019-07-08 16:47:12 +0300

    fix(TDI-42615): Add placeholder for SAS in datastore (#179)

--- azure/azureblob/src/main/java/org/talend/components/azure/datastore/AzureCloudConnection.java
+++ azure/azureblob/src/main/java/org/talend/components/azure/datastore/AzureCloudConnection.java
@@ -47,7 +47,7 @@ public class AzureCloudConnection implements Serializable {
     private boolean useAzureSharedSignature;
 
     @Option
-    @Documentation("Enter your account SAS URL between double quotation marks.\n"
+    @Documentation("Enter your account SAS URL.\n"
             + "You can get the SAS URL for each allowed service on Microsoft Azure portal after generating SAS.\n"
             + "The SAS URL format is https://<$storagename>.<$service>.core.windows.net/<$sastoken>, "
             + "where <$storagename> is the storage account name, "
