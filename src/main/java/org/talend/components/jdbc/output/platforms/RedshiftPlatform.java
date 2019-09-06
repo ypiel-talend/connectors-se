411636373c3566f0cc8debd3f315927394ae53d2
Anas KHABALI <akhabali@talend.com>
2019-09-06 17:09:24 +0200

    redshift support single sort key (#130)
    * redshift support single sort key
    
    * rebase branch on master

--- src/main/java/org/talend/components/jdbc/output/platforms/RedshiftPlatform.java
+++ src/main/java/org/talend/components/jdbc/output/platforms/RedshiftPlatform.java
@@ -60,12 +60,15 @@ public class RedshiftPlatform extends Platform {
         sql.append(identifier(table.getName()));
         sql.append("(");
         List<Column> columns = table.getColumns();
-        sql.append(createColumns(columns));
+        sql.append(createColumns(columns, table.getSortStrategy(), columns.stream().filter(Column::isSortKey).collect(toList())));
         sql.append(createPKs(table.getName(), columns.stream().filter(Column::isPrimaryKey).collect(toList())));
         sql.append(")");
         sql.append(createDistributionKeys(table.getDistributionStrategy(),
                 columns.stream().filter(Column::isDistributionKey).collect(toList())));
-        sql.append(createSortKeys(table.getSortStrategy(), columns.stream().filter(Column::isSortKey).collect(toList())));
+        if (RedshiftSortStrategy.COMPOUND.equals(table.getSortStrategy())
+                || RedshiftSortStrategy.INTERLEAVED.equals(table.getSortStrategy())) {
+            sql.append(createSortKeys(table.getSortStrategy(), columns.stream().filter(Column::isSortKey).collect(toList())));
+        }
 
         log.debug("### create table query ###");
         log.debug(sql.toString());
@@ -99,15 +102,16 @@ public class RedshiftPlatform extends Platform {
         return e instanceof SQLException && "23505".equals(((SQLException) e).getSQLState());
     }
 
-    private String createColumns(final List<Column> columns) {
-        return columns.stream().map(this::createColumn).collect(Collectors.joining(","));
+    private String createColumns(final List<Column> columns, final RedshiftSortStrategy sortStrategy,
+            final List<Column> sortKeys) {
+        return columns.stream().map(c -> createColumn(c, sortStrategy, sortKeys)).collect(Collectors.joining(","));
     }
 
-    private String createColumn(final Column column) {
+    private String createColumn(final Column column, final RedshiftSortStrategy sortStrategy, final List<Column> sortKeys) {
         return identifier(column.getName())//
                 + " " + toDBType(column)//
                 + " " + isRequired(column)//
-        ;
+                + (RedshiftSortStrategy.SINGLE.equals(sortStrategy) && sortKeys.contains(column) ? "" : " sortkey");
     }
 
     private String toDBType(final Column column) {
