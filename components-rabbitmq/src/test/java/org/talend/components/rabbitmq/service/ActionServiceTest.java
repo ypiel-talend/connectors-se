add14e5a06db9f74da4308abeb3588723ae2dda9
vizotenko <vizotenko@talend.com>
2019-09-20 11:01:44 +0300

    feat(TDI-41172): update javadoc

--- components-rabbitmq/src/test/java/org/talend/components/rabbitmq/service/ActionServiceTest.java
+++ components-rabbitmq/src/test/java/org/talend/components/rabbitmq/service/ActionServiceTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.talend.components.rabbitmq.service;
 
 import org.junit.jupiter.api.Test;
@@ -36,16 +48,6 @@ class ActionServiceTest {
         HealthCheckStatus status = actionService.validateBasicDatastore(dataStore);
 
         assertEquals(HealthCheckStatus.Status.KO, status.getStatus());
-    }
-
-    @Test
-    public void testGuessSchema() {
-        Schema schema = actionService.guessSchema(new BasicConfiguration());
-        assertNotNull(schema, "Guess Schema should not be null");
-        Optional<Schema.Entry> optional = schema.getEntries().stream().findFirst();
-        assertTrue(optional.isPresent(), "Guess Schema Entry was not set");
-        assertEquals(MESSAGE_CONTENT, optional.get().getName());
-        assertEquals(Schema.Type.STRING, optional.get().getType());
     }
 
     @Test
