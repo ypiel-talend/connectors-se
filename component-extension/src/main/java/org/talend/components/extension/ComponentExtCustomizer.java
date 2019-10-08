ba913e78a8bbdb80dd64bd739df6af812b79de20
Rémy Dubois <rdubois@talend.com>
2019-10-08 13:48:43 +0200

    Bump connectors version to 1.3.0-SNAPSHOT

--- component-extension/src/main/java/org/talend/components/extension/ComponentExtCustomizer.java
+++ component-extension/src/main/java/org/talend/components/extension/ComponentExtCustomizer.java
@@ -10,7 +10,6 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-
 package org.talend.components.extension;
 
 import org.talend.sdk.component.runtime.manager.ComponentManager;
