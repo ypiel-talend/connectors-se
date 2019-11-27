08e6f24823452de0783d7a19af603e232ca1b963
rlecomte-talend <rlecomte@talend.com>
2019-11-27 11:54:47 +0100

    TDI-41443 : migrating bigquery to TCK
    Fixing issues in connection settings

--- bigquery/src/main/java/org/talend/components/bigquery/datastore/BigQueryConnection.java
+++ bigquery/src/main/java/org/talend/components/bigquery/datastore/BigQueryConnection.java
@@ -42,6 +42,7 @@ public class BigQueryConnection implements Serializable {
 
     @Option
     @Credential
+    @Required
     @Documentation("Google credential (JSON)")
     private String jsonCredentials;
 
