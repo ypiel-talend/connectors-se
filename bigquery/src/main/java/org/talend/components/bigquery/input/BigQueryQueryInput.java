134cc726e640aad877c8183e97022c9faa2da1eb
rlecomte-talend <rlecomte@talend.com>
2019-11-28 14:02:11 +0100

    TDI-41443 : migrating bigquery to TCK
    Refactor + TU

--- bigquery/src/main/java/org/talend/components/bigquery/input/BigQueryQueryInput.java
+++ bigquery/src/main/java/org/talend/components/bigquery/input/BigQueryQueryInput.java
@@ -98,39 +98,7 @@ public class BigQueryQueryInput implements Serializable {
             Record.Builder rb = builderFactory.newRecordBuilder(tckSchema);
 
             for (Field f : tableSchema.getFields()) {
-                String name = f.getName();
-                FieldValue value = fieldValueList.get(name);
-
-                if (value != null && !value.isNull()) {
-                    LegacySQLTypeName type = f.getType();
-
-                    switch (type.name()) {
-                    case "BOOLEAN":
-                        rb.withBoolean(name, value.getBooleanValue());
-                        break;
-                    case "BYTES":
-                        rb.withBytes(name, value.getBytesValue());
-                        break;
-                    case "DATE":
-                        rb.withDateTime(name, new Date(value.getTimestampValue()));
-                        break;
-                    case "DATETIME":
-                        rb.withDateTime(name, new Date(value.getTimestampValue()));
-                        break;
-                    case "FLOAT":
-                        rb.withDouble(name, value.getDoubleValue());
-                        break;
-                    case "INTEGER":
-                        rb.withLong(name, value.getLongValue());
-                        break;
-                    case "TIME":
-                        rb.withLong(name, value.getLongValue());
-                        break;
-                    default:
-                        rb.withString(name, value.getStringValue());
-                    }
-                }
-
+                service.convertToTckField(fieldValueList, rb, f);
             }
 
             record = rb.build();
