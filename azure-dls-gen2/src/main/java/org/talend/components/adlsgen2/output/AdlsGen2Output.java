5341f5146b40e60db50b4a5e333dadc1202da252
ypiel <ypiel@talend.com>
2020-01-03 13:50:14 +0100

    feat(TDI-42337) : adls gen2 icons

--- azure-dls-gen2/src/main/java/org/talend/components/adlsgen2/output/AdlsGen2Output.java
+++ azure-dls-gen2/src/main/java/org/talend/components/adlsgen2/output/AdlsGen2Output.java
@@ -39,7 +39,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Version(1)
-@Icon(value = Icon.IconType.CUSTOM, custom = "AdlsGen2Output")
+@Icon(value = Icon.IconType.CUSTOM, custom = "azure-datalake")
 @Processor(name = "AdlsGen2Output")
 @Documentation("Azure Data Lake Storage Gen2 Output")
 public class AdlsGen2Output implements Serializable {
