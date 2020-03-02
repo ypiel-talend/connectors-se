fbbf34a49b4b6b96481d70fc3787b2737ee14f7e
clesaec <51320496+clesaec@users.noreply.github.com>
2020-03-02 15:34:30 +0100

    Revert "Clesaec/tdi 43520 google storage (#326)" (#344)
    This reverts commit a2ca5d727ebd9dc3bf10971cfbc89c73feda70f6.

--- google-storage/src/main/java/org/talend/components/google/storage/package-info.java
+++
@@ -1,8 +0,0 @@
-// this tells the framework in which family (group of components) and categories (UI grouping)
-// the components in the nested packages belong to
-@Components(family = "GoogleStorage", categories = { "Business", "Cloud" })
-@Icon(value = Icon.IconType.CUSTOM, custom = "cloudstorageInput")
-package org.talend.components.google.storage;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
