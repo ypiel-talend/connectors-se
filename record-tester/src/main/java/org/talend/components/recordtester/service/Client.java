b01db4313123bed4c897f823db944a3dabc89961
ypiel <ypiel@talend.com>
2020-04-20 16:13:19 +0200

    Add record-tester

---
+++ record-tester/src/main/java/org/talend/components/recordtester/service/Client.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2006-2020 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.talend.components.recordtester.service;
+
+import org.talend.sdk.component.api.service.http.Codec;
+import org.talend.sdk.component.api.service.http.ConfigurerOption;
+import org.talend.sdk.component.api.service.http.Headers;
+import org.talend.sdk.component.api.service.http.HttpClient;
+import org.talend.sdk.component.api.service.http.HttpMethod;
+import org.talend.sdk.component.api.service.http.QueryParams;
+import org.talend.sdk.component.api.service.http.Request;
+import org.talend.sdk.component.api.service.http.Response;
+import org.talend.sdk.component.api.service.http.Url;
+import org.talend.sdk.component.api.service.http.UseConfigurer;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public interface Client extends HttpClient {
+
+    @Request
+    Response<byte[]> execute(@HttpMethod String httpMethod, @Url String url, @Headers Map<String, String> headers,
+            @QueryParams(/* default encode = true */) Map<String, String> queryParams);
+
+}
