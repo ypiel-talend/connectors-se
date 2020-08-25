82b1bf19cf7b5cbaa7f03ef15d4f2ac3e34dc6fd
Richard Lecomte <rlecomte@talend.com>
2020-08-25 11:11:52 +0200

    Rlecomte/tdi 44729 move ftp to connectors ee (#457)
    * TDI-44729 : Move FTP connector to connectors-ee
    
    * TDI-44729 : Move FTP connector to connectors-ee

--- ftp/src/main/java/org/talend/components/ftp/package-info.java
+++
@@ -1,6 +0,0 @@
-@Components(family = "FTP", categories = "Internet")
-@Icon(value = Icon.IconType.CUSTOM, custom = "ftp")
-package org.talend.components.ftp;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
