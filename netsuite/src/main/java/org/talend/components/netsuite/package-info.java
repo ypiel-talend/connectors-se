ea87b6ee095bba6ceb5a35e93a0dc812c2451c01
Dmytro Grygorenko <59691438+dmytrogrygorenko@users.noreply.github.com>
2020-10-14 08:55:21 +0300

    fix(TDI-44797): Remove NetSuite (moved to connectors-ee) (#462)

--- netsuite/src/main/java/org/talend/components/netsuite/package-info.java
+++
@@ -1,6 +0,0 @@
-@Components(family = "NetSuite", categories = "Business")
-@Icon(value = Icon.IconType.CUSTOM, custom = "NetSuite")
-package org.talend.components.netsuite;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
