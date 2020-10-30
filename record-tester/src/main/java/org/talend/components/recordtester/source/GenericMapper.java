8c3f998714badf21f6b7988ec6a34e0bd533a6c7
ypiel <ypiel@talend.com>
2020-10-30 17:24:45 +0100

    Add a mapperand a split option.

--- record-tester/src/main/java/org/talend/components/recordtester/source/GenericMapper.java
+++ record-tester/src/main/java/org/talend/components/recordtester/source/GenericMapper.java
@@ -14,48 +14,58 @@ package org.talend.components.recordtester.source;
 
 import lombok.extern.slf4j.Slf4j;
 import org.talend.components.recordtester.conf.Config;
-import org.talend.components.recordtester.conf.Datastore;
 import org.talend.components.recordtester.service.GenericService;
 import org.talend.sdk.component.api.component.Icon;
 import org.talend.sdk.component.api.component.Version;
 import org.talend.sdk.component.api.configuration.Option;
-import org.talend.sdk.component.api.input.Producer;
+import org.talend.sdk.component.api.input.Assessor;
+import org.talend.sdk.component.api.input.Emitter;
+import org.talend.sdk.component.api.input.PartitionMapper;
+import org.talend.sdk.component.api.input.PartitionSize;
+import org.talend.sdk.component.api.input.Split;
 import org.talend.sdk.component.api.meta.Documentation;
 
 import java.io.Serializable;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class GenericEmitter implements Serializable {
+@Slf4j
+@Version(1)
+@Icon(Icon.IconType.STAR)
+@PartitionMapper(name = "genericInput")
+@Documentation("")
+public class GenericMapper implements Serializable {
 
     private final Config config;
 
     private GenericService service;
 
-    private transient boolean done = false;
-
-    private Iterator<Object> records;
-
-    public GenericEmitter(@Option("configuration") final Config config, final GenericService service) {
+    public GenericMapper(@Option("configuration") final Config config, final GenericService service) {
         this.service = service;
         this.config = config;
     }
 
-    @Producer
-    public Object next() {
-        if (done) {
-            return null;
-        }
-
-        if (records == null) {
-            records = service.get(config).iterator();
-        }
-
-        if (!records.hasNext()) {
-            done = true;
-            return null;
-        }
-
-        return records.next();
+    @Assessor
+    public long estimateSize() {
+        return 0;
     }
 
+    @Split
+    public List<GenericMapper> split(@PartitionSize final long bundleSize) {
+        Integer splits = config.getDataset().getSplits();
+        if (config.isOverwriteDataset()) {
+            splits = config.getSplits();
+        }
+
+        log.info("GenericMapper nb split : " + splits);
+
+        return IntStream.range(0, splits).mapToObj(i -> new GenericMapper(config, service)).collect(Collectors.toList());
+    }
+
+    @Emitter
+    public GenericEmitter createSource() {
+        return new GenericEmitter(config, service);
+    }
 }
