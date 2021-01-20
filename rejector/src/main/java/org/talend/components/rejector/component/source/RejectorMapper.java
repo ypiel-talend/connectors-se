9746ff1c2d986b69ba9118c98023902d639380d4
undx <egallois@talend.com>
2021-01-20 11:16:16 +0100

    feat(TCOMP-1824): add processor for testing reject flow in Studio

---
+++ rejector/src/main/java/org/talend/components/rejector/component/source/RejectorMapper.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2006-2021 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.talend.components.rejector.component.source;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.talend.sdk.component.api.component.Icon;
+import org.talend.sdk.component.api.component.Icon.IconType;
+import org.talend.sdk.component.api.component.Version;
+import org.talend.sdk.component.api.configuration.Option;
+import org.talend.sdk.component.api.input.Assessor;
+import org.talend.sdk.component.api.input.Emitter;
+import org.talend.sdk.component.api.input.PartitionMapper;
+import org.talend.sdk.component.api.input.PartitionSize;
+import org.talend.sdk.component.api.input.Split;
+import org.talend.sdk.component.api.meta.Documentation;
+
+@PartitionMapper(name = "RejectorSource")
+@Icon(value = IconType.CUSTOM, custom = "rejector")
+@Version(1)
+@Documentation("Mapper for Rejector.")
+public class RejectorMapper implements Serializable {
+
+    private final RejectorInputConfiguration configuration;
+
+    public RejectorMapper(@Option("configuration") final RejectorInputConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    @Assessor
+    public long estimateSize() {
+        return 1L;
+    }
+
+    @Split
+    public List<RejectorMapper> split(@PartitionSize final long desiredSize) {
+        return null;
+    }
+
+    @Emitter
+    public RejectorGenerator createWorker() {
+        return null;
+    }
+}
