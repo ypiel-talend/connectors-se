ae66565b067fcc99a5bfe4081adc331b772f0e0e
rlecomte-talend <rlecomte@talend.com>
2021-01-26 10:40:49 +0100

    POC reject

--- rejector/src/main/java/org/talend/components/rejector/component/processor/TestProcessor.java
+++ rejector/src/main/java/org/talend/components/rejector/component/processor/TestProcessor.java
@@ -10,70 +10,41 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package org.talend.components.rejector.component.sink;
+package org.talend.components.rejector.component.processor;
 
 import java.io.Serializable;
-
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
+import java.util.Random;
 
 import org.talend.components.rejector.service.I18nMessages;
 import org.talend.sdk.component.api.component.Icon;
-import org.talend.sdk.component.api.component.Icon.IconType;
 import org.talend.sdk.component.api.component.Version;
-import org.talend.sdk.component.api.configuration.Option;
 import org.talend.sdk.component.api.meta.Documentation;
-import org.talend.sdk.component.api.processor.AfterGroup;
-import org.talend.sdk.component.api.processor.BeforeGroup;
 import org.talend.sdk.component.api.processor.ElementListener;
 import org.talend.sdk.component.api.processor.Output;
 import org.talend.sdk.component.api.processor.OutputEmitter;
 import org.talend.sdk.component.api.processor.Processor;
 import org.talend.sdk.component.api.record.Record;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Processor(name = "RejectorProcessor")
-@Icon(value = IconType.CUSTOM, custom = "rejector")
+@Processor(name = "RejectorProcessorTest")
+@Icon(value = Icon.IconType.CUSTOM, custom = "rejector")
 @Version(1)
 @Documentation("A connector for testing rejects in Studio.")
-public class RejectorProcessor implements Serializable {
+@RequiredArgsConstructor
+public class TestProcessor implements Serializable {
 
-    private final RejectorProcessorConfiguration configuration;
+    private final TestProcessorConfiguration configuration;
 
     private final I18nMessages i18n;
 
-    public RejectorProcessor(@Option("configuration") final RejectorProcessorConfiguration configuration,
-            final I18nMessages messages) {
-        this.configuration = configuration;
-        this.i18n = messages;
-    }
-
-    @PostConstruct
-    public void init() {
-    }
-
-    @PreDestroy()
-    public void release() {
-    }
-
-    @BeforeGroup
-    public void begin() {
-    }
-
     @ElementListener
-    public void bufferize(final Record data
-    // , @Output("main") OutputEmitter<Record> main
-            , @Output("REJECT") OutputEmitter<Record> reject) {
-        // main.emit(data);
-        reject.emit(data);
-        log.info(data.toString());
-
-    }
-
-    @AfterGroup
-    public void commit() {
+    public void bufferize(final Record data, @Output("main") OutputEmitter<Record> main
+    // ,@Output("REJECT") OutputEmitter<Record> reject
+    ) {
+        main.emit(data);
     }
 
 }
