1dffa4bfcd46aaf2af6a1f06a7d20599c7f4af1b
ypiel <ypiel@talend.com>
2021-04-21 18:29:02 +0200

    chore : bumpt to tck:1.32.0

--- azure-dls-gen2/src/test/java/org/talend/components/adlsgen2/runtime/output/BlobWriterTest.java
+++ azure-dls-gen2/src/test/java/org/talend/components/adlsgen2/runtime/output/BlobWriterTest.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2006-2021 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package org.talend.components.adlsgen2.runtime.output;
 
 import org.junit.jupiter.api.Assertions;
@@ -7,7 +19,7 @@ import org.talend.components.adlsgen2.AdlsGen2TestBase;
 import org.talend.sdk.component.junit5.WithComponents;
 
 @WithComponents("org.talend.components.adlsgen2")
-public class BlobWriterTest extends AdlsGen2TestBase{
+public class BlobWriterTest extends AdlsGen2TestBase {
 
     static BlobWriter blobWriter;
 
@@ -20,10 +32,10 @@ public class BlobWriterTest extends AdlsGen2TestBase{
 
     @Test
     public void testCompleteOnZeroInputRecords() {
-        /* 
+        /*
          * There was a NPE issue.
          * No batch was created, since the {@link BlobWriter#newBatch()} was not called.
-        */
+         */
         Assertions.assertDoesNotThrow(blobWriter::complete);
     }
 }
