8b63e64d589520ec97cb1d6d2a8c81376eea9e15
mbasiuk <mbasiuk@talend.com>
2021-09-27 19:52:50 +0300

    chore(TDI-46073): add extra echo

--- .jenkins/changelog.sh
+++ .jenkins/changelog.sh
@@ -7,14 +7,13 @@ git config --global credential.helper '!echo password=${GITHUB_TOKEN}; echo'
 git config --global credential.name "jenkins-build"
 echo "Fetching all tags."
 git fetch --tags
-
+echo "Release version for now ${RELEASE_VERSION}"
 echo "Getting last commit sha."
 if [[ ${BRANCH_NAME} == 'master' ]]; then
     NEW_MAINTENANCE="maintenance/${RELEASE_VERSION%.*}"
     LAST_COMMIT_SHA=$(git log --format="%H" origin/${NEW_MAINTENANCE}...master | head -n -1 | tail -n 1)
     NEW_RELEASE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout | cut -d- -f1)
 else
-  echo "Release version for now ${RELEASE_VERSION}"
   # pushed to related origin maintenance branch
   MAJOR=$(echo ${RELEASE_VERSION} | cut -d. -f1)
   MINOR=$(echo ${RELEASE_VERSION} | cut -d. -f2)
@@ -24,14 +23,16 @@ else
   LAST_COMMIT_SHA=$(git log --format="%H" release/${PREVIOUS_RELEASE_VERSION}...release/${NEW_RELEASE_VERSION} | head -n -1 | tail -n 1)
 fi
 
+echo "Last commit sha - ${LAST_COMMIT_SHA}"
 IS_DRAFT=true
 
-cd ..
-git clone https://github.com/Talend/connectivity-tools.git
-cd connectivity-tools
-git checkout mbasiuk/TDI-46073_release_note_app
-cd release-notes
-mvn clean install #package
-cd target
-JAR_NAME=$(find . -maxdepth 1 -name "*.jar" | cut -d/ -f2)
-java -jar $JAR_NAME $NEW_RELEASE_VERSION $REPOSITORY $LAST_COMMIT_SHA
+#Cannot clone repository
+#cd ..
+#git clone https://github.com/Talend/connectivity-tools.git
+#cd connectivity-tools
+#git checkout mbasiuk/TDI-46073_release_note_app
+#cd release-notes
+#mvn clean install #package
+#cd target
+#JAR_NAME=$(find . -maxdepth 1 -name "*.jar" | cut -d/ -f2)
+#java -jar $JAR_NAME $NEW_RELEASE_VERSION $REPOSITORY $LAST_COMMIT_SHA
