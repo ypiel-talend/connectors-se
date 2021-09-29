9b609a7252f5939e64ce25293be130e7b149f8e8
mbasiuk <mbasiuk@talend.com>
2021-09-29 17:33:14 +0300

    chore(TDI-46073): remove some logging, update usage of last commit sha

--- .jenkins/changelog.sh
+++ .jenkins/changelog.sh
@@ -6,33 +6,32 @@ git config --global credential.username ${GITHUB_LOGIN}
 git config --global credential.helper '!echo password=${GITHUB_TOKEN}; echo'
 git config --global credential.name "jenkins-build"
 echo "Fetching all tags."
-git fetch --tags
+#Too many unnecessary logged info
+git fetch --tags > /dev/null
 echo "Release version for now ${RELEASE_VERSION}"
 echo "Getting last commit sha."
 if [[ ${BRANCH_NAME} == 'master' ]]; then
     NEW_MAINTENANCE="maintenance/${RELEASE_VERSION%.*}"
     LAST_COMMIT_SHA=$(git log --format="%H" origin/${NEW_MAINTENANCE}...master | head -n -1 | tail -n 1)
-    NEW_RELEASE_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:3.2.0:evaluate -Dexpression=project.version -q -DforceStdout | cut -d- -f1)
 else
   # pushed to related origin maintenance branch
   MAJOR=$(echo ${RELEASE_VERSION} | cut -d. -f1)
   MINOR=$(echo ${RELEASE_VERSION} | cut -d. -f2)
   PATCH=$(($(echo ${RELEASE_VERSION} | cut -d. -f3) - 1))
   PREVIOUS_RELEASE_VERSION=${MAJOR}.${MINOR}.${PATCH}
-  NEW_RELEASE_VERSION=$RELEASE_VERSION
   LAST_COMMIT_SHA=$(git log --format="%H" release/${PREVIOUS_RELEASE_VERSION}...release/${NEW_RELEASE_VERSION} | head -n -1 | tail -n 1)
 fi
 
 echo "Last commit sha - ${LAST_COMMIT_SHA}"
 echo "Draft - ${DRAFT}"
-echo "New release version - ${NEW_RELEASE_VERSION}"
-#Cannot clone repository
-cd ..
+echo "New release version - ${RELEASE_VERSION}"
+
+cd .. && \
 git clone https://github.com/Talend/connectivity-tools.git && \
 cd connectivity-tools && \
 git checkout mbasiuk/TDI-46073_release_note_app && \
 cd release-notes && \
 mvn clean package && \
-cd target && \
+cd target
 JAR_NAME=$(find . -maxdepth 1 -name "*.jar" | cut -d/ -f2)
-java -jar ${JAR_NAME} ${NEW_RELEASE_VERSION} ${REPOSITORY} ${LAST_COMMIT_SHA}
+java -jar ${JAR_NAME} ${LAST_COMMIT_SHA}
