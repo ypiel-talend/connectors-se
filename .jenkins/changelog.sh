732362fbf32ad13688414a17d8a6c7b2bfeb8281
mbasiuk <mbasiuk@talend.com>
2021-09-30 19:06:15 +0300

    chore(TDI-46073): test master flow

--- .jenkins/changelog.sh
+++ .jenkins/changelog.sh
@@ -3,26 +3,23 @@
 echo "Fetching all tags."
 #Too many unnecessary logged info
 git fetch --tags -q
-echo "Release version for now ${RELEASE_VERSION}"
+echo "Release version ${RELEASE_VERSION}"
 echo "Getting last commit sha."
-if [[ ${BRANCH_NAME} == 'master' ]]; then
-    NEW_MAINTENANCE="maintenance/${RELEASE_VERSION%.*}"
-    LAST_COMMIT_SHA=$(git log --format="%H" origin/${NEW_MAINTENANCE}...master | head -n -1 | tail -n 1)
+if [[ ${BRANCH_NAME} -ne 'master' ]]; then
+    RELEASE_VERSION=1.25.0
+    LAST_COMMIT_SHA=$(git log --format="%H" origin/maintenance/${RELEASE_VERSION%.*}...master | head -n -1 | tail -n 1)
 else
   # pushed to related origin maintenance branch
-  RELEASE_VERSION=1.24.2
   MAJOR=$(echo ${RELEASE_VERSION} | cut -d. -f1)
   MINOR=$(echo ${RELEASE_VERSION} | cut -d. -f2)
   PATCH=$(($(echo ${RELEASE_VERSION} | cut -d. -f3) - 1))
   PREVIOUS_RELEASE_VERSION=${MAJOR}.${MINOR}.${PATCH}
   LAST_COMMIT_SHA=$(git log --format="%H" release/${PREVIOUS_RELEASE_VERSION}...release/${RELEASE_VERSION} | head -n -1 | tail -n 1)
-  #Commented to test changes.
-  #$(git log --format="%H" release/${PREVIOUS_RELEASE_VERSION}...release/${NEW_RELEASE_VERSION} | head -n -1 | tail -n 1)
 fi
 
+echo "Previous release version ${PREVIOUS_RELEASE_VERSION}"
 echo "Last commit sha - ${LAST_COMMIT_SHA}"
 echo "Draft - ${DRAFT}"
-echo "New release version - ${RELEASE_VERSION}"
 
 cd .. && \
 git clone https://github.com/Talend/connectivity-tools.git && \
