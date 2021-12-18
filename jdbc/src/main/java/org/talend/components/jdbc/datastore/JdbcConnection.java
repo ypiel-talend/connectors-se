74ab6ab4ce87418c5b3b1b217c514cdf7979df41
ypiel <ypiel@talend.com>
2021-12-18 01:38:17 +0100

    feat(TDI-46935): increment configuration version

--- jdbc/src/main/java/org/talend/components/jdbc/datastore/JdbcConnection.java
+++ jdbc/src/main/java/org/talend/components/jdbc/datastore/JdbcConnection.java
@@ -58,7 +58,7 @@ import lombok.ToString;
 @Documentation("A connection to a data base.")
 public class JdbcConnection implements Serializable {
 
-    public final static int VERSION = 3;
+    public final static int VERSION = 4;
 
     @Option
     @Required
