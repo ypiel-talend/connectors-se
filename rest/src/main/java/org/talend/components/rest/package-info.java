32c26a392b3b90739a4d773210903f84cb9f3b47
ypiel <ypiel@talend.com>
2021-12-23 17:42:54 +0100

    chore(TDI-45142): more rest to -ee

--- rest/src/main/java/org/talend/components/rest/package-info.java
+++
@@ -1,6 +0,0 @@
-@Components(family = "REST", categories = "Internet")
-@Icon(value = Icon.IconType.CUSTOM, custom = "rest-family")
-package org.talend.components.rest;
-
-import org.talend.sdk.component.api.component.Components;
-import org.talend.sdk.component.api.component.Icon;
